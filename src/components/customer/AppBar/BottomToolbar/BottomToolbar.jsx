393357dc8cd3b321f340d832e5553627468652ea
nazhifsetya27 <nazhif.nugroho@gmail.com>
2023-11-22 12:12:32 +0700

    Update BottomToolbar.jsx

--- src/components/customer/AppBar/BottomToolbar/BottomToolbar.jsx
+++ src/components/customer/AppBar/BottomToolbar/BottomToolbar.jsx
@@ -1,9 +1,14 @@
 import { Box, Toolbar } from '@mui/material';
+import { useEffect } from 'react';
 import CustomerAddressButton from './CustomerAddressButton';
 import { BackToHome } from './BackToHome';
 
 function BottomToolbar() {
   const isUserLogin = localStorage.getItem('token');
+
+  useEffect(() => {
+    localStorage.getItem('token');
+  }, []);
 
   return (
     <Toolbar variant="dense" sx={{ minHeight: 'fit-content' }}>
