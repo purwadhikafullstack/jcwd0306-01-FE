09dacd7a5fe7c9b6c3de75bff90b70fe5a2edfec
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-11-28 23:09:21 +0700

    fix(WarehouseDetailPage): fix searchParams

--- src/components/admin/WarehouseDetailPage/StockMutation/StockMutationContainer.jsx
+++ src/components/admin/WarehouseDetailPage/StockMutation/StockMutationContainer.jsx
@@ -5,11 +5,55 @@ import {
   AccordionSummary,
   Stack,
 } from '@mui/material';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import SearchInput from './SearchInput';
 import StockMutationTableFooter from './StockMutationTableFooter';
 import StockMutationTable from './StockMutationTable';
+import { asyncGetStockMutations } from '../../../../states/stockMutations/action';
 
 function StockMutationContainer() {
+  const warehouse = useSelector((states) => states.warehouse);
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setSearchParams({
+      tab: searchParams.get('tab'),
+      search: searchParams.get('search') || '',
+      sortBy: 'updatedAt',
+      orderBy: 'desc',
+      page: 1,
+      perPage: 10,
+    });
+    return setSearchParams;
+  }, []);
+
+  useEffect(() => {
+    dispatch(
+      asyncGetStockMutations({
+        search: searchParams.get('search'),
+        status: searchParams.get('status'),
+        type: searchParams.get('type'),
+        sortBy: searchParams.get('sortBy') || 'updatedAt',
+        orderBy: searchParams.get('orderBy') || 'desc',
+        page: searchParams.get('page') || 1,
+        perPage: searchParams.get('perPage') || 10,
+        warehouseId: warehouse.id,
+      })
+    );
+  }, [
+    dispatch,
+    warehouse,
+    searchParams.get('status'),
+    searchParams.get('type'),
+    searchParams.get('sortBy'),
+    searchParams.get('orderBy'),
+    searchParams.get('page'),
+    searchParams.get('perPage'),
+  ]);
+
   return (
     <Accordion defaultExpanded>
       <AccordionSummary
--- src/pages/admin/WarehouseDetailPage.jsx
+++ src/pages/admin/WarehouseDetailPage.jsx
@@ -1,65 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Box, Typography } from '@mui/material';
-import { asyncGetProducts } from '../../states/products/action';
 import Container from '../../components/admin/WarehouseDetailPage/Container';
 import { asyncGetWarehouse } from '../../states/warehouse/action';
 import BgImage from '../../assets/background1.svg';
-import { asyncGetStockMutations } from '../../states/stockMutations/action';
 
 function WarehouseDetailPage() {
   const warehouse = useSelector((states) => states.warehouse);
   const dispatch = useDispatch();
   const { warehouseId } = useParams();
-  const [searchParams] = useSearchParams();
 
   useEffect(() => {
     dispatch(asyncGetWarehouse(warehouseId));
   }, [dispatch, warehouseId]);
-
-  useEffect(() => {
-    const currentTab = searchParams.get('tab');
-    if (currentTab === 'products') {
-      dispatch(
-        asyncGetProducts({
-          getType: 'REPLACE',
-          search: searchParams.get('search'),
-          categoryId: searchParams.get('categoryId'),
-          sortBy: searchParams.get('sortBy') || 'updatedAt',
-          orderBy: searchParams.get('orderBy') || 'desc',
-          paranoid: false,
-          page: searchParams.get('page') || 1,
-          perPage: searchParams.get('perPage') || 10,
-          warehouseId,
-        })
-      );
-    } else if (currentTab === 'stock-mutations') {
-      dispatch(
-        asyncGetStockMutations({
-          search: searchParams.get('search'),
-          status: searchParams.get('status'),
-          type: searchParams.get('type'),
-          sortBy: searchParams.get('sortBy') || 'updatedAt',
-          orderBy: searchParams.get('orderBy') || 'desc',
-          page: searchParams.get('page') || 1,
-          perPage: searchParams.get('perPage') || 10,
-          warehouseId,
-        })
-      );
-    }
-  }, [
-    dispatch,
-    warehouseId,
-    searchParams.get('tab'),
-    searchParams.get('categoryId'),
-    searchParams.get('sortBy'),
-    searchParams.get('orderBy'),
-    searchParams.get('page'),
-    searchParams.get('perPage'),
-    searchParams.get('status'),
-    searchParams.get('type'),
-  ]);
 
   if (warehouse === null) return null;
 
