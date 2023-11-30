3ed58c171a4ac5bfa539f3e322bfd3bfb3688de8
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-11-30 08:17:36 +0700

    feat: add skeleton UI

--- src/components/customer/HomePage/ProductCardList.jsx
+++ src/components/customer/HomePage/ProductCardList.jsx
@@ -1,10 +1,11 @@
-import { ImageList, Stack, Typography } from '@mui/material';
+import { ImageList, Skeleton, Stack, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import ProductCardItem from './ProductCardItem';
 import MoreProductsButton from './MoreProductsButton';
 
 function ProductCardList() {
   const products = useSelector((states) => states.products);
+  const { default: loading } = useSelector((states) => states.loadingBar);
 
   return (
     <Stack>
@@ -20,6 +21,19 @@ function ProductCardList() {
         {products.map((product) => (
           <ProductCardItem key={product.id} product={product} />
         ))}
+        {loading > 0 &&
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((val) => (
+            <Stack
+              key={val}
+              sx={{ minWidth: '10rem', borderRadius: 1, overflow: 'hidden' }}
+            >
+              <Skeleton variant="rectangular" width="100%" height="10rem" />
+              <Skeleton variant="text" width="100%" />
+              <Skeleton variant="text" width="80%" />
+              <Skeleton variant="text" width="40%" />
+              <Skeleton variant="text" width="60%" />
+            </Stack>
+          ))}
       </ImageList>
       {products.length > 0 ? (
         <MoreProductsButton />
--- src/components/customer/ProductPage/ProductStack.jsx
+++ src/components/customer/ProductPage/ProductStack.jsx
@@ -1,10 +1,11 @@
-import { ImageList, Stack, Typography } from '@mui/material';
+import { ImageList, Skeleton, Stack, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import ProductCardItem from '../HomePage/ProductCardItem';
 import PaginationStack from './PaginationStack';
 
 function ProductStack() {
   const products = useSelector((states) => states.products);
+  const { default: loading } = useSelector((states) => states.loadingBar);
 
   return (
     <Stack>
@@ -20,6 +21,19 @@ function ProductStack() {
         {products.map((product) => (
           <ProductCardItem key={product.id} product={product} />
         ))}
+        {loading > 0 &&
+          [1, 2, 3, 4, 5].map((val) => (
+            <Stack
+              key={val}
+              sx={{ minWidth: '10rem', borderRadius: 1, overflow: 'hidden' }}
+            >
+              <Skeleton variant="rectangular" width="100%" height="10rem" />
+              <Skeleton variant="text" width="100%" />
+              <Skeleton variant="text" width="80%" />
+              <Skeleton variant="text" width="40%" />
+              <Skeleton variant="text" width="60%" />
+            </Stack>
+          ))}
       </ImageList>
       {products.length > 0 ? (
         <PaginationStack />
